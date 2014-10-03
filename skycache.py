d5e2f14e00bfd8fa7b28e4c05098f4a3d951695c
Nick Doiron <ndoiron@mapmeld.com>
2014-10-02 22:09:47 -0400

    attempt output tile urls

--- skycache.py
+++ skycache.py
@@ -1,9 +1,10 @@
-import web
+import os, web
 import view, config
 from view import render
 
 urls = (
-  '/', 'index'
+  '/', 'index',
+  '/uploaded/(.*)', 'uploaded'
 )
 
 class index:
@@ -12,14 +13,22 @@ class index:
 
   def POST(self):
     form = web.input(tiles={})
-    filedir = '~/skycache/static'
+    filedir = '/root/skycache/tiles'
     if 'tiles' in form:
       filepath = form.tiles.filename.replace('\\','/')
       filename = filepath.split('/')[-1]
       fout = open(filedir + '/' + filename, 'w')
       fout.write(form.tiles.file.read())
       fout.close()
-    raise web.seeother('/')
+      arg = str(len(os.listdir('static')) + 2)
+      os.system('mb-util ' + filedir + '/' + filename + ' /root/skycache/static/tile' + arg)
+      raise web.seeother('/uploaded/tile' + arg)
+    else:
+      raise web.seeother('/')
+
+class uploaded:
+  def GET(self, tilevar):
+    return render.uploaded(web.websafe(tilevar))
 
 if __name__ == "__main__":
   app = web.application(urls, globals())
