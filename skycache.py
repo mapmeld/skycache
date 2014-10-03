c650e09c6c89f99ce2bdfc87d1821ab83ddb581e
Nick Doiron <ndoiron@mapmeld.com>
2014-10-02 21:50:48 -0400

    attempt upload MBTiles

--- skycache.py
+++ skycache.py
@@ -10,6 +10,17 @@ class index:
   def GET(self):
     return render.base('hello world')
 
+  def POST(self):
+    form = web.input(tiles={})
+    filedir = '~/skycache/static'
+    if 'tiles' in form:
+      filepath = form.tiles.filename.replace('\\','/')
+      filename = filepath.split('/')[-1]
+      fout = open(filedir + '/' + filename, 'w')
+      fout.write(form.tiles.file.read())
+      fout.close()
+    raise web.seeother('/')
+
 if __name__ == "__main__":
   app = web.application(urls, globals())
   app.internalerror = web.debugerror
